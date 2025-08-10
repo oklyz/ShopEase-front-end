b640fab363552b95e405fd03ee5fe4f9f42f3ad9
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-10 21:59:02 +0300

    done with order details componets

--- ShopEase/src/components/OrderDetails.jsx
+++ ShopEase/src/components/OrderDetails.jsx
@@ -1,15 +1,22 @@
 import { useOrders } from '../contexts/OrdersContext'
 import { useParams } from 'react-router-dom'
 
-// import UserOrderContext from '../contexts/UserOrderContext'
 const OrderDetails = () => {
   const { orders } = useOrders()
   const { index } = useParams()
-  // const { userOrder } = useContext(UserOrderContext)
+
   console.log(orders.orders[index])
+
   return (
     <>
-      <h1>{index}</h1>
+      {orders.orders[index].items.map((items, index) => {
+        return (
+          <div className="items" key={items._id}>
+            <h2>Item Name: {items.name}</h2>
+            <h2>Price: {items.price}</h2>
+          </div>
+        )
+      })}
     </>
   )
 }
--- ShopEase/src/components/OrderList.jsx
+++ ShopEase/src/components/OrderList.jsx
@@ -19,30 +19,21 @@ const OrderList = () => {
 
   return (
     <>
-      <div className="items">
-        {!UserOrder ? (
-          <h1>'No orders found for this user'</h1>
-        ) : (
-          // console.log(UserOrder.orders[0])
-          // console.log(UserOrder.orders[0].items[0])
-
-          UserOrder.orders.map((order, index) => {
-            return (
-              <Link to={`/profile/order/${index}`} className="orderlink">
-                <div className="OrdersList" key={order._id}>
-                  <h2>Order No: {index + 1}</h2>
-                  <h2>Date: {order.date}</h2>
-                  <h2>Number of Items: {order.items.length}</h2>
-                </div>
-              </Link>
-            )
-          })
-
-          // UserOrder.orders.map((order) => {
-          //   return <h1>{order.name}</h1>
-          // })
-        )}
-      </div>
+      {!UserOrder ? (
+        <h1>'No orders found for this user'</h1>
+      ) : (
+        UserOrder.orders.map((order, index) => {
+          return (
+            <Link to={`/profile/order/${index}`} className="orderlink">
+              <div className="OrdersList" key={order._id}>
+                <h2>Order No: {index + 1}</h2>
+                <h2>Date: {order.date.split('T')[0]}</h2>
+                <h2>Number of Items: {order.items.length}</h2>
+              </div>
+            </Link>
+          )
+        })
+      )}
     </>
   )
 }
