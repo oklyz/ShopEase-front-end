c56da02b0862b0eb41ed0cd56ce2a08d1111fc46
oklyz <alimodffs2@gmail.com>
2025-08-10 12:36:52 +0300

    save Changes

--- ShopEase/src/pages/Home.jsx
+++ ShopEase/src/pages/Home.jsx
@@ -1,10 +1,34 @@
+import Search from "../components/Search"
+import { useState, useEffect } from "react"
+import { GetItems } from "../services/item"
+
 const Home = () => {
   
+  const [search, setSearch] = useState("")
+  const [searchResult, setSearchResult] = useState()
   
+  useEffect(() => {
+    GetItems()
+  },[])
+
+  console.log(GetItems())
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSearchResult(search)
+    setSearch("")
+  }
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+  
+  
+
   return(
     <>
     <div>
-      Home
+      <Search handleChange={handleChange} handleSubmit={handleSubmit}/>
     </div>
     </>
   )
