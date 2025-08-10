c6520368f4cdecf7b369d2e822ee2ad852f97bed
zeshan2001 <zeshanbh@gmail.com>
2025-08-10 09:22:25 +0300

    header, register, and login done

--- ShopEase/src/components/Header.jsx
+++ ShopEase/src/components/Header.jsx
@@ -1,19 +1,59 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import UserContext from '../contexts/UserContext'
 
 const Nav = () => {
+  const { user, setUser } = useContext(UserContext)
 
-  
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const adminOptions = (
+    <nav>
+      <Link to="">Dashboard</Link>
+      <Link to="">Issues</Link>
+      <Link to="">New Product</Link>
+      <Link to="/login" onClick={handleLogOut}>
+        log out
+      </Link>
+    </nav>
+  )
+
+  const customerOptions = (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="">Items</Link>
+      <Link to="">Contact</Link>
+      <Link to="">Cart</Link>
+      <Link to="/login" onClick={handleLogOut}>
+        log out
+      </Link>
+      <Link to="">Profile</Link>
+    </nav>
+  )
+
+  const publicOptions = (
+    <nav>
+      <Link to="/login">log in</Link>
+      <Link to="/register">Sign up</Link>
+    </nav>
+  )
 
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/login">Sign in</Link>
-      <Link to="/register">Sign up</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/itemDetails">ItemDetails</Link>
+      <header>
+        {user ? (
+          <>
+            {user.role === 'customer' && <>{customerOptions}</>}
+            {user.role === 'admin' && <>{adminOptions}</>}
+          </>
+        ) : (
+          publicOptions
+        )}
+      </header>
     </>
   )
 }
--- ShopEase/src/pages/Home.jsx
+++ ShopEase/src/pages/Home.jsx
@@ -3,7 +3,9 @@ const Home = () => {
   
   return(
     <>
-
+    <div>
+      Home
+    </div>
     </>
   )
 }
