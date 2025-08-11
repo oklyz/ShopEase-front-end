a18ddb528de9b2b9ad2acdd16c602041e2496305
Alderazi <qm8222606@gmail.com>
2025-08-11 10:22:51 +0300

    calling item by id from back-end

--- ShopEase/src/pages/ItemDetails.jsx
+++ ShopEase/src/pages/ItemDetails.jsx
@@ -1,7 +1,24 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { getItemById } from "../services/item"
 const ItemDetails = () => {
-  
-  
+
+  let {itemId} = useParams()
+  console.log(itemId)
+
+  const [item,setItem]=useState()
+useEffect(()=> {
+  const getItemDetails=async()=>{
+    const itemdetails= await getItemById(itemId)  
+  setItem(itemdetails)
+
+  }
+getItemDetails()
+},[])
+
+
   return(
+    
     <>
 
     </>
