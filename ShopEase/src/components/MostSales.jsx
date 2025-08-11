e0deb75b9a5be833fd4a8bf7b968532625b463af
oklyz <alimodffs2@gmail.com>
2025-08-11 14:20:41 +0300

    Make a function that sort item from the height to the lowest

--- ShopEase/src/components/MostSales.jsx
+++ ShopEase/src/components/MostSales.jsx
@@ -14,12 +14,23 @@ const MostSales = () => {
     getItem()
   },[])
   
-  item &&console.log(item)
   
+  const sortedArray = (item) => {
+    let newArray = item
+    const sortedArray = newArray.sort((a, b) => {
+      return b.numberOfSold - a.numberOfSold
+    })
+    return sortedArray
+  }
+
+  const sortedItem = sortedArray(item)
+
+
+
   return(
     <>
       <h1>MostSales</h1>
-      {item.map((item) => (
+      {sortedItem.map((item) => (
             <Items item={item} key={item._id} />
       ))}
     </>
