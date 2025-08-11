a9139fe9db60010791cbe1948b1815b5b72fe583
zeshan2001 <zeshanbh@gmail.com>
2025-08-11 21:19:53 +0300

    img added in AdminItemDetails

--- ShopEase/src/components/AdminItemDetails.jsx
+++ ShopEase/src/components/AdminItemDetails.jsx
@@ -1,7 +1,15 @@
+import { BASE_URL } from '../services/api'
+
 const AdminItemDetails = ({ item }) => {
   return (
     <>
       <div>
+        <img
+          src={`${BASE_URL}/images/${item.image}`}
+          alt="no image"
+          width={100}
+          height={100}
+        />
         <h1>{item.name}</h1>
         <p>{item.price}</p>
         <p>{item.quantity}</p>
