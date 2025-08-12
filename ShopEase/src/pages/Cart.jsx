51aadbb96cdd13d19acc121334d3b0a855bca1d4
zeshan2001 <zeshanbh@gmail.com>
2025-08-12 12:34:34 +0300

    display cart info in cart page

--- ShopEase/src/pages/Cart.jsx
+++ ShopEase/src/pages/Cart.jsx
@@ -1,9 +1,30 @@
-import { useContext } from "react"
-import CartContext from "../contexts/CartContext"
+import { useContext } from 'react'
+import CartContext from '../contexts/CartContext'
 const Cart = () => {
+  const { cart } = useContext(CartContext)
   return (
     <>
-      <div>Cart...</div>
+      {cart.length > 0 ? (
+        <>
+          <div>
+            {cart.map((item) => (
+              <>
+                <h3>{item.name}</h3>
+                <h3>{item.price}</h3>
+                <h3>{item.quantityOrdered}</h3>
+              </>
+            ))}
+          </div>
+          <br />
+          <br />
+        </>
+      ) : (
+        <>
+          <div>
+            <h1>Empty</h1>
+          </div>
+        </>
+      )}
     </>
   )
 }
