600135b871257a5f4ee9215e626d6b0c2df6a943
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-12 11:22:40 +0300

    add user name for comments

--- ShopEase/src/components/DisplayComments.jsx
+++ ShopEase/src/components/DisplayComments.jsx
@@ -1,9 +1,30 @@
-const DisplayComments =({comment})=>{
-return(
+import { UserInfo } from '../services/Auth'
+import { useState, useEffect } from 'react'
+
+const DisplayComments = ({ comment }) => {
+  const [UserData, setUserData] = useState(null)
+
+  useEffect(() => {
+    const getDataUser = async () => {
+      const dataUser = await UserInfo(comment.userId)
+      setUserData(dataUser)
+    }
+    getDataUser()
+  }, [])
+  return (
     <>
-    <h2>{comment.description}</h2>
-    <h4>{comment.rate}</h4>
+      {UserData ? (
+        <>
+          <div>
+            <h2>Name :{UserData.name}</h2>
+            <h2>Description: {comment.description}</h2>
+            <h4>Rate: {comment.rate}</h4>
+          </div>
+        </>
+      ) : (
+        <>loading...</>
+      )}
     </>
-)
+  )
 }
 export default DisplayComments
