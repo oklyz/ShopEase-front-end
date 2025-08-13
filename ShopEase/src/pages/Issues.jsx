b08f8f177f2917854376781b73b5848478305ff1
zeshan2001 <zeshanbh@gmail.com>
2025-08-13 23:51:43 +0300

    item form styling and dash

--- ShopEase/src/pages/Issues.jsx
+++ ShopEase/src/pages/Issues.jsx
@@ -19,81 +19,17 @@ const Issues = () => {
     <>
       {issues.length > 0 ? (
         <div className="issues-container">
-          {/* {issues.map((issue) => {
-            return (
-              <div key={issue._id}>
-                <p>{issue.subject}</p>
-                <p>{issue.description}</p>
-                <p>{issue.order}</p>
+          {issues.map((issue) => (
+            <div className="cookie-card" key={issue._id}>
+              <span className="title">🍪 {issue.subject}</span>
+              <p className="description">{issue.description}</p>
+              <div className="actions">
+                <span>Order ID:</span>
+                <button className="pref">{issue.order}</button>
+                {/* <button className="accept">Accept</button> */}
               </div>
-            )
-          })} */}
-
-          <div className="cookie-card">
-            <span className="title">🍪 Cookie Notice</span>
-            <p className="description">
-              We use cookies to ensure that we give you the best experience on
-              our website Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Praesentium, aperiam!
-            </p>
-            <div className="actions">
-              <span>Order ID:</span>
-              <button className="pref">GFREH46VG5G6BH49F56</button>
-              {/* <button className="accept">Accept</button> */}
             </div>
-          </div>
-          <div className="cookie-card">
-            <span className="title">🍪 Cookie Notice</span>
-            <p className="description">
-              We use cookies to ensure that we give you the best experience on
-              our website Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Praesentium, aperiam!
-            </p>
-            <div className="actions">
-              <span>Order ID:</span>
-              <button className="pref">GFREH46VG5G6BH49F56</button>
-              {/* <button className="accept">Accept</button> */}
-            </div>
-          </div>
-          <div className="cookie-card">
-            <span className="title">🍪 Cookie Notice</span>
-            <p className="description">
-              We use cookies to ensure that we give you the best experience on
-              our website Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Praesentium, aperiam!
-            </p>
-            <div className="actions">
-              <span>Order ID:</span>
-              <button className="pref">GFREH46VG5G6BH49F56</button>
-              {/* <button className="accept">Accept</button> */}
-            </div>
-          </div>
-          <div className="cookie-card">
-            <span className="title">🍪 Cookie Notice</span>
-            <p className="description">
-              We use cookies to ensure that we give you the best experience on
-              our website Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Praesentium, aperiam!
-            </p>
-            <div className="actions">
-              <span>Order ID:</span>
-              <button className="pref">GFREH46VG5G6BH49F56</button>
-              {/* <button className="accept">Accept</button> */}
-            </div>
-          </div>
-          <div className="cookie-card">
-            <span className="title">🍪 Cookie Notice</span>
-            <p className="description">
-              We use cookies to ensure that we give you the best experience on
-              our website Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Praesentium, aperiam!
-            </p>
-            <div className="actions">
-              <span>Order ID:</span>
-              <button className="pref">GFREH46VG5G6BH49F56</button>
-              {/* <button className="accept">Accept</button> */}
-            </div>
-          </div>
+          ))}
         </div>
       ) : (
         <>
